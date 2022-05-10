8b87d29d610ab6cbca6c6ccd36a9182999cb6557
Ahmed Rifat <rifatbafsc@gmail.com>
2022-05-10 16:46:28 +0600

    navbar added

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>This is login Page</h1>
+            <h1>This is login Page</h1>
+            <h1>This is login Page</h1>
+            <h1>This is login Page</h1>
+            <h1>This is login Page</h1>
+            <h1>This is login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
